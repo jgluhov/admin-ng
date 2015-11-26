112ea2ed622405dd0aedcd9b8c824ff4531ad8e8
jgluhov <jgluhov@gmail.com>
2015-11-26 16:02:25 +0300

    change app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var app = require('express')();
-var server = require('http').Server(app);
-server.listen(80);
+var express = require('express'),
+  path = require('path'),
+  logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-console.log('Express server started on port %s', server.address().port);
+
+var port = app.listen(process.env.PORT || 3000);
+app.listen(port);
