71afc1d3bd4fb027e116e69a6221e0b5720d9429
jgluhov <jgluhov@gmail.com>
2015-11-26 15:20:31 +0300

    force add bower libs

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = require('express')();
+var server = require('http').Server(app);
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+console.log('Express server started on port %s', server.address().port);
